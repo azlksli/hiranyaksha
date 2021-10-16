d821e832f6fa09473e76469f4fa86c49196db0c7
azlksli <19683394802qq.com>
2021-10-16 19:40:56 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
+// import './plugins/element.js'
+// import element from 'element-ui'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+app.use(store).use(router).mount('#app')
