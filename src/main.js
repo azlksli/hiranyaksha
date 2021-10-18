0d4e39e4ddddc704c54c9905645360b354f4f66a
azlksli <1969339480@qq.com>
2021-10-18 19:08:36 +0800

    第二次

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
-// import './plugins/element.js'
-// import element from 'element-ui'
+import Axios from 'axios'
+import './plugins/element.js'
+
+Axios.defaults.baseURL="http://127.0.0.1/"
 
 const app = createApp(App)
 installElementPlus(app)
+app.config.globalProperties.$http=Axios;  //this.$http.
 app.use(store).use(router).mount('#app')
